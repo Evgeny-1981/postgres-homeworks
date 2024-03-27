fec264d0460d13f906023f1dba58d114955517bc
Evgeny Egorov <EAEgorov42@gmail.com>
2024-03-27 16:58:30 +0500

    Домашнее задание №1 по СУБД

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,25 @@
--- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	employee_id serial PRIMARY KEY,
+	first_name varchar(20) NOT NULL,
+	last_name varchar(20) NOT NULL,
+	title text,
+	birth_date date NOT NULL,
+	notes text
+);
+
+CREATE TABLE customers
+(
+	customer_id varchar(12) PRIMARY KEY,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(50) NOT NULL
+);
+
+CREATE TABLE orders_data
+(
+	order_id serial PRIMARY KEY,
+	customer_id varchar(10) REFERENCES customers(customer_id) NOT NULL,
+	employee_id int REFERENCES employees(employee_id) NOT NULL,
+	order_date date NOT NULL,
+	ship_city varchar(30) NOT NULL
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,28 @@
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
+# """Скрипт для заполнения данными таблиц в БД Postgres."""
+import os
+import pandas as pd
+import psycopg2
+
+# """задаем параметры для подключения к БД."""
+conn_params = {
+    'dbname': 'north',
+    'user': 'postgres',
+    'password': '1238',
+}
+
+with psycopg2.connect(**conn_params) as conn:
+    with conn.cursor() as cur:
+        # Считываем данные из файла CSV
+        file1 = pd.read_csv(os.path.join(os.path.dirname(__file__), 'north_data', "customers_data.csv"))
+        file2 = pd.read_csv(os.path.join(os.path.dirname(__file__), 'north_data', "employees_data.csv"))
+        file3 = pd.read_csv(os.path.join(os.path.dirname(__file__), 'north_data', "orders_data.csv"))
+        # data_files = (file1, file2, file3)
+        # Заполнение таблицы данными
+        for row in file1.values:
+            cur.execute("INSERT INTO customers VALUES (%s, %s, %s)", row)
+        for row in file2.values:
+            cur.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)", row)
+        for row in file3.values:
+            cur.execute("INSERT INTO orders_data VALUES (%s, %s, %s, %s, %s)", row)
+    cur.close()
+conn.commit()
